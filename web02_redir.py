833da528e5a4390a35284ca672a0a88849ac6fc0
Jaromrax <jaromrax@gmail.com>
2017-05-26 17:24:16 +0200

    BIG rewrite: 1/ ALL REDIRECTS; 2/ if /nastro=> replace /nastro and work from home; All jpgs

--- web02_redir.py
+++ web02_redir.py
@@ -7,30 +7,36 @@ import cgi
 import datetime
 import logging
 import argparse
+import string
 
 import re
+import glob  # jpegs
 ###================================ end of logging config
 
+
+REDIRECT_TO="www.spiral2.eu"
 PORT_NUMBER = 8011
 WEBHOME='/home/spiral2/SharedDisk/spiral2-web/'
 WEBHOME='/home/ojr/Maps'
 
-parser=argparse.ArgumentParser(description=""" 
+parser=argparse.ArgumentParser(description="""
 """)
 
 parser.add_argument('-w','--webhome',  default="/home/spiral2/SharedDisk/spiral2-web" , help='')
 parser.add_argument('-p','--port',  default=8011,type=int , help='')
+parser.add_argument('-r','--redirect_to',  default="spiral2.ujf.cas.cz", help='')
 
-args=parser.parse_args() 
+args=parser.parse_args()
 WEBHOME=args.webhome
 PORT_NUMBER=args.port
+REDIRECT_TO=args.redirect_to
 
 
 ### LOGGER ====================================
 formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
 def setup_logger(name, log_file, level=logging.INFO):
     """Function setup as many loggers as you want"""
-    handler = logging.FileHandler(log_file)        
+    handler = logging.FileHandler(log_file)
     handler.setFormatter(formatter)
 
     logger = logging.getLogger(name)
@@ -41,122 +47,185 @@ def setup_logger(name, log_file, level=logging.INFO):
 
 log = setup_logger("normal",WEBHOME+'/web02.log')
 loH = setup_logger("detail",WEBHOME+'/web02_details.log')
-log.info('Program started')
+log.info('=======================Program started==========================')
 
 
+
+### GLOB ===============================
+def PrepareJPGS(WEBHOME, prefix):
+
+    COLUMNS=2
+    WIDTH=450
+    
+    lines=[]
+    topline=["| "]*(COLUMNS)
+    midline=["|-"]*(COLUMNS) 
+    #print( "".join(topline) )
+    #print( "".join(midline) )
+    
+    photos=" ".join(topline) + '|\n' + " ".join(midline) + ' |\n'
+    photos=""
+    i=1
+    for jpg in glob.glob(WEBHOME+"/*.jpg"):
+        njpg=prefix+'/'+os.path.basename(jpg)
+        print('i... ',i,'.  JPG FILE', jpg, njpg )
+        line='<a href="'+njpg+'" target="_blank"><img src="'+njpg+'" width="'+str(WIDTH)+'"></img></a>'
+        #line='<img src="'+njpg+'" width="'+str(450)+'"></img>'
+        lines.append( line )
+        if i%COLUMNS==0:
+            print('i... preparing line')
+            photos=photos+"| "+"|".join(lines)+'|\n <br> \n <br>\n'
+            lines=[]
+        i=i+1
+
+    if len(lines)!=0:
+        print('i... preparing last incomplete line')
+        photos=photos+"| "+"|".join(lines)+'|\n'
+    
+    print(photos)
+    return photos
+
+#================================== CLASS  HANDLER========
 #This class will handles any incoming request from
-#the browser 
+#the browser
 class myHandler(BaseHTTPRequestHandler):
 
-        #Handler for the GET requests
-        def do_GET(self):
-                log.info( datetime.datetime.now().strftime("%Y%m%d_%a_%H%M%S")+' '+self.client_address[0]+' '+'#'+' '+self.requestline )
-                loH.info( self.headers)
-                #print('GET ', end='')
-                #self.path=
-                #print( re.sub( r'\.\.','', self.path ) )
-                if self.path.find('\.\.')>=0:
-                        print('ERROR .. ', self.path)
-                        log.error(' .. ')
-                        return
-                if self.path.find('/')!=0:
-                        print('ERROR / ', self.path)
-                        log.error(' / ')
-                        return
-                if self.path=="/":
-                        self.path="/index.html"
-                        response='<meta http-equiv="refresh" content="0; url=http://example.com/" />'
-                try:
-                        #Check the file extension required and
-                        #set the right mime type
-                        sendFile = False
-                        if self.path.endswith(".html"):
-                                mimetype='text/html'
-                                sendFile = True
-                        if self.path.endswith(".jpg"):
-                                mimetype='image/jpg'
-                                sendFile = True
-                        if self.path.endswith(".gif"):
-                                mimetype='image/gif'
-                                sendFile = True
-                        if self.path.endswith(".ico"):
-                                mimetype='image/ico'
-                                sendFile = True
-                        if self.path.endswith(".js"):
-                                mimetype='application/javascript'
-                                sendFile = True
-                        if self.path.endswith(".css"):
-                                mimetype='text/css'
-                                sendFile = True
-                        #if sendFile and (os.path.isfile(WEBHOME+self.path)):
-                        #        print('sendFile at get  = ',sendFile, '. Filename=',self.path)
-                        #else:
-                        #        print('no sendfile')
-                        if sendFile and (os.path.isfile(WEBHOME+self.path)):
-                                f = open(WEBHOME + self.path,'rb')      
-                                print('serving = '+str( f.name )+'  '+str(not(f.closed)))
-                                self.send_response(200)
-                                self.send_header('Content-type',mimetype)
-                                self.end_headers()
-                                self.wfile.write(f.read())
-                                f.close()
-                        elif sendFile:
-                                print('....file doesnot exist...')
-                                log.error('file doesnot exist')
-                                self.send_error(404,'File Not Found: %s' % self.path)
-                        else:
-                                # MY OWN CODES =================================
-                                if self.path=='/nastro/':
-                                        print('NASTRO')
-                                        self.send_response(200)
-                                        self.send_header('Content-type','text/html')
-                                        self.end_headers()
-                                        response='HELLO. THIS SCRIPT SHOWS inline http code and is example for a server with GET(normal html) and POST(form sending) PARTS<hr>'
-                                        self.wfile.write(
-                                        bytes('<html><body>'+response+'</body></html>','utf-8'))
-                                else:
-                                        log.error('Unknown CODE')
-                        return
+    #Handler for the GET requests ====+++++GET++++=====
+    def do_GET(self):
+        """
+        when redirected: client_addres=127.0.0.1
+        X-Forwarded-For:
+        """
+        # FAKE SERVER VERSION
+        self.server_version = "Server: Apache 13.2"
+        self.sys_version = ""
+        #LOG STUFF
+        remoteIP=re.findall('X-Forwarded-For:\s+([\d\.]+)', str(self.headers) )
+        #remoteIP=re.findall(r'Connection:(.+)', str(self.headers) )
+        print( 'remoteIP=',remoteIP )
+        log.info( datetime.datetime.now().strftime(" %a  - ")+self.client_address[0]+' '.join(remoteIP)+' # '+self.requestline )
+#        log.info( datetime.datetime.now().strftime("%Y%m%d_%a_%H%M%S")+' '+self.client_address[0]+' '+ '#' +' '+self.requestline )
+        loH.info( self.headers)
+        #print('GET ', end='')
+        #self.path=
+        #print( re.sub( r'\.\.','', self.path ) )
+        ###############################
+        # basic security /amateur level/ :  \b backspaces
+        #  .. path
+        self.path=self.path.replace( '\b', '')
+        self.path=self.path.replace( '..', '')
+        if self.path.find('\.\.')>=0:
+            print('ERROR .. ', self.path)
+            log.error(' .. found')
+            self.send_error(404,'File Not Found: %s' % self.path)
+            return
+        if self.path.find('/')!=0:
+            print('ERROR / ', self.path)
+            log.error(' / ')
+            self.send_error(404,'File Not Found: %s' % self.path)
+            return
+        if self.path=="/":  # just translate
+            self.path="/index.html"
+        #===== now: if not nastro:redirect =================
+        #print('i... before nastro check', self.path)
+        if self.path.find('/nastro'):
+            #self.send_error(404,'File Not Found: %s' % self.path)
+            #return
+            self.send_response(301)
+            self.send_header('Location','http://'+REDIRECT_TO)
+            log.info(' ... redirected to '+REDIRECT_TO)
+            self.end_headers()
+            return
+        #===== now: if not nastro:redirect =================
+        #print('i... succeeded to NASTRO', self.path)
+        self.path=re.sub('/nastro','', self.path )
+        if len(self.path)==0: self.path='/'
+        print('i... succeeded to NASTRO', self.path)
+        #if self.path=="/":  # just translate
+        #    self.path="/index.html"
+        try:
+            sendFile = False
+            if self.path.endswith(".html"):
+                mimetype='text/html'
+                sendFile = True
+            if self.path.endswith(".jpg"):
+                mimetype='image/jpg'
+                sendFile = True
+            if self.path.endswith(".gif"):
+                mimetype='image/gif'
+                sendFile = True
+            if self.path.endswith(".ico"):
+                mimetype='image/ico'
+                sendFile = True
+            if self.path.endswith(".js"):
+                mimetype='application/javascript'
+                sendFile = True
+            if self.path.endswith(".css"):
+                mimetype='text/css'
+                sendFile = True
+            if sendFile and (os.path.isfile(WEBHOME+self.path)):
+                f = open(WEBHOME + self.path,'rb')
+                print('serving = '+str( f.name )+'  '+str(not(f.closed)))
+                self.send_response(200)
+                self.send_header('Content-type',mimetype)
+                self.end_headers()
+                self.wfile.write(f.read())
+                f.close()
+            elif sendFile:
+                print('....file doesnot exist...')
+                log.error('file doesnot exist')
+                self.send_error(404,'File Not Found: %s' % self.path)
+            else:
+                # MY OWN CODES =================================
+                print('NASTRO - own code')
+                self.send_response(200)
+                self.send_header('Content-type','text/html')
+                self.end_headers()
+                response=PrepareJPGS(WEBHOME,'/nastro')
+                self.wfile.write(
+                    bytes('<html><body>'+response+'</body></html>','utf-8'))
+            return
 
-                except IOError:
-                        print('send ioerror')
-                        self.send_error(404,'File Not Found: %s' % self.path)
+        except IOError:
+            print('send ioerror')
+            self.send_error(404,'File Not Found: %s' % self.path)
 
-        #Handler for the POST requests
-        def do_POST(self):
-                print('POST entered : path=',self.path )
-                log.error('Unallowed POST ')
-                return
-                # if self.path=="/send":   # this is a section in POST
-                #         form = cgi.FieldStorage(
-                #                 fp=self.rfile, 
-                #                 headers=self.headers,
-                #                 environ={'REQUEST_METHOD':'POST',
-                #                  'CONTENT_TYPE':self.headers['Content-Type'],
-                #         })
-                #         print( form.keys() )
-                #         print( "Your name is: %s" % form["your_name"].value)
-                #         self.send_response(200)
-                #         self.send_header('Content-type','text/html')
-                #         self.end_headers()
-                #         aaa=''
-                #         for k in form.keys() :
-                #                 aaa=aaa + form[k].value +'\n' ;
-                #                 print( k, form[ k].value )
-                #         self.wfile.write( bytes('<html><body>'+
-                #                 'WE ARE IN POST PART NOW<br><br>Thanks '+ form["your_name"].value + '. <br>'+
-                #                 str(form.keys())+aaa+
-                #                 '</body></html>','utf-8') )
-                #         return
+    #Handler for the POST requests
+    def do_POST(self):
+        print('POST entered : path=',self.path )
+        log.error('Unallowed POST ')
+        self.send_error(404,'File Not Found: %s' % self.path)
+        return
+        # if self.path=="/send":   # this is a section in POST
+        #         form = cgi.FieldStorage(
+        #                 fp=self.rfile,
+        #                 headers=self.headers,
+        #                 environ={'REQUEST_METHOD':'POST',
+        #                  'CONTENT_TYPE':self.headers['Content-Type'],
+        #         })
+        #         print( form.keys() )
+        #         print( "Your name is: %s" % form["your_name"].value)
+        #         self.send_response(200)
+        #         self.send_header('Content-type','text/html')
+        #         self.end_headers()
+        #         aaa=''
+        #         for k in form.keys() :
+        #                 aaa=aaa + form[k].value +'\n' ;
+        #                 print( k, form[ k].value )
+        #         self.wfile.write( bytes('<html><body>'+
+        #                 'WE ARE IN POST PART NOW<br><br>Thanks '+ form["your_name"].value + '. <br>'+
+        #                 str(form.keys())+aaa+
+        #                 '</body></html>','utf-8') )
+        #         return
 try:
-        #Create a web server and define the handler to manage the
-        #incoming request
-        server = HTTPServer(('', PORT_NUMBER), myHandler)
-        print( 'Started httpserver on port ' , PORT_NUMBER)
-        
-        #Wait forever for incoming htto requests
-        server.serve_forever()
+    #Create a web server and define the handler to manage the
+    #incoming request
+    server = HTTPServer(('', PORT_NUMBER), myHandler)
+    print( 'Started httpserver on port ' , PORT_NUMBER)
+
+    #Wait forever for incoming htto requests
+    server.serve_forever()
 
 except KeyboardInterrupt:
-        print( '^C received, shutting down the web server')
-        server.socket.close()
+    print( '^C received, shutting down the web server')
+    server.socket.close()
