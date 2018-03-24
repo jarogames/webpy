22c7dcc777700f5e66ee8b8151b7b8a622e2112a
Jaromrax <jaromrax@gmail.com>
2018-03-24 17:56:01 +0100

    25100:static:  variable with server time; debug;  ALLWORLD 0.0.0.0

--- 20180312_flask_staticpage_25100/server.py
+++ 20180312_flask_staticpage_25100/server.py
@@ -13,6 +13,8 @@ from random import randint
 import time
 from shutil import copyfile  # COPY FROM TO dir (not best solution)
 import os,sys
+import datetime #  to show now
+
 
 HTTP_PORT = 25100
 ZMQ_LISTENING_PORT = HTTP_PORT+1000
@@ -20,6 +22,8 @@ ZMQ_LISTENING_PORT = HTTP_PORT+1000
 monkey.patch_all()  # for ZMQ
 
 app = Flask(__name__) #app = Flask(__name__, static_url_path = "/tmp", static_folder = "tmp" ) 
+app.config['DEBUG'] =  True  # DO NOT USE IN PRODUCTION  !!!!!!!!!!!!
+
 
 logging.basicConfig(level=logging.INFO)  # INFO / DEBUG
 logger = logging.getLogger(__name__)
@@ -31,7 +35,9 @@ context = zmq.Context()
 @app.route('/')
 def index():   # gets from ./templates/
     logger.info('Rendering index page')
-    return render_template('index.html')
+    return render_template('index.html', var=datetime.datetime.now()  )
+
+
 
 ###############################################################
 #
@@ -43,6 +49,6 @@ if __name__ == '__main__':
     logger.info('Launching web server;  port='+str(HTTP_PORT)+' zmq_port='+str(ZMQ_LISTENING_PORT) )
     from gevent import pywsgi
     from geventwebsocket.handler import WebSocketHandler
-    server = pywsgi.WSGIServer(('', HTTP_PORT), app, handler_class=WebSocketHandler)
+    server = pywsgi.WSGIServer(('0.0.0.0', HTTP_PORT), app, handler_class=WebSocketHandler)
     server.serve_forever()
     
